4d9ce193cb10c8e94a0433ae5555614791e2955b
Demian Volkov <demianwolfssd@gmail.com>
2025-08-04 06:04:44 +0100

    Add static factories to RAII

--- include/linalg_core.hpp
+++ include/linalg_core.hpp
@@ -21,8 +21,13 @@ namespace linalg {
             Opt() : ptr(init_optstruct()) {}
 
             operator optstruct*() { return ptr.get(); }
+            operator const optstruct*() const { return ptr.get(); }
+            
             optstruct* get() { return ptr.get(); }
             const optstruct* get() const { return ptr.get(); }
+            
+            optstruct* operator->() { return ptr.get(); }
+            const optstruct* operator->() const { return ptr.get(); }
 
             // prevent copying [because unique_ptr]
             Opt(const Opt&) = delete;
@@ -31,6 +36,30 @@ namespace linalg {
             // allow moving
             Opt(Opt&&) = default;
             Opt& operator=(Opt&&) = default;
+
+            static Opt binary32() {
+                Opt o;
+                o->precision = 24;
+                o->emin = -126;
+                o->emax = 127;
+                return o;
+            }
+
+            static Opt binary64() {
+                Opt o;
+                o->precision = 53;
+                o->emin = -1022;
+                o->emax = 1023;
+                return o;
+            }
+
+            static Opt custom(int prec, int emin, int emax) {
+                Opt o;
+                o->precision = prec;
+                o->emin = emin;
+                o->emax = emax;
+                return o;
+            }
     };
     
     int cpfloat_add(double *X, const double *A, const double *B,
